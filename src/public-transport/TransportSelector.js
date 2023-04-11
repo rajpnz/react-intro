1a09c22fdecfa9b616c1768ae002face9bfb4dff
RPatel <rpatel@linz.govt.nz>
2023-04-11 12:38:11 +1200

    Add the selector to select what train/bus stop the user is interested in

--- src/public-transport/TransportSelector.js
+++ src/public-transport/TransportSelector.js
@@ -1,8 +1,21 @@
 import {useState} from "react";
 const MODES = ["Train", "Bus"];
+const TRAIN_STOPS = [{stop_id: 1, name: "Johnsonville"}, {stop_id: 2, name: "Raroa"}, {stop_id: 3, name: "Wellington"}]
+const BUS_STOPS = [{stop_id: 4, name: "Johnsonville Mall"}, {stop_id: 3252, name: "BP Johnsonville"},
+    {stop_id: 5502, name: "Lambton Quay"}, {stop_id: 5, name: "Wellington Station"}]
 
 function TransportSelector() {
     const [mode, setMode] = useState("");
+    const [stop, setStop] = useState("");
+    const [stops, setStops] = useState([]);
+
+    function setUpStops(typeOfTransport){
+        if (typeOfTransport === "Train") {
+            setStops(TRAIN_STOPS);
+        } else if (typeOfTransport === "Bus") {
+            setStops(BUS_STOPS)
+        }
+    }
 
     return (
         <div className="transport-selector">
@@ -15,15 +28,33 @@ function TransportSelector() {
                     value={mode}
                     onChange={(e) => {
                         setMode(e.target.value);
+                        setUpStops(e.target.value);
                     }}
                     onBlur={(e) => {
                         setMode(e.target.value);
                     }}
                 >
-                    <option />
                     {MODES.map((oneMode) => (
                         <option key={oneMode} value={oneMode}>
                             {oneMode}
+                        </option>
+                    ))}
+                </select>
+                <br/>
+                <label htmlFor="stop">
+                    Stop
+                </label>
+                <select
+                    disabled={!stops.length}
+                    id="stop"
+                    value={stop}
+                    onChange={(e) => setStop(e.target.value)}
+                    onBlur={(e) => setStop(e.target.value)}
+                >
+                    {stops.map((oneStop) =>
+                        (
+                        <option key={oneStop.stop_id} value={oneStop.name}>
+                            {oneStop.name}
                         </option>
                     ))}
                 </select>
