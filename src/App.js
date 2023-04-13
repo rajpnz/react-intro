fbdd954b7e8f1d56822fdaadceb75fe38f0cdee1
RPatel <rpatel@linz.govt.nz>
2023-04-14 11:55:13 +1200

    Remove the counter from the user interface

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Counter from './counter/Counter'
 import TransportSelector from "./public-transport/TransportSelector";
 
 function App() {
@@ -9,9 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-        <div>
-            <Counter />
-        </div>
+        <div></div>
         <div>
             <TransportSelector />
         </div>
