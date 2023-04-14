9a45c051a9b0daaf45cc42b84be2c3abdbdd7a57
RPatel <rpatel@linz.govt.nz>
2023-04-14 14:33:07 +1200

    Basic tabulation of results

--- src/public-transport/TransportSelector.js
+++ src/public-transport/TransportSelector.js
@@ -104,11 +104,24 @@ function TransportSelector() {
                 </select>
             </form>
             <br/>
-            {departures?.map((departure) => (
-                <div>
-                    <span>{`${departure.service_id} ---- ${departure.status} ----- ${departure.destination.name} ---- ${departure.departure.expected}`}</span>
-                </div>
-            ))}
+            <div>
+                <table>
+                    <tr>
+                        <th>Service ID</th>
+                        <th>Status</th>
+                        <th>Destination</th>
+                        <th>Expected Departure</th>
+                    </tr>
+                    {departures?.map((departure) => (
+                        <tr>
+                            <td>{departure.service_id}</td>
+                            <td>{departure.status}</td>
+                            <td>{departure.destination.name}</td>
+                            <td>{departure.departure.expected}</td>
+                        </tr>
+                    ))}
+                </table>
+            </div>
         </div>
     );
 }
