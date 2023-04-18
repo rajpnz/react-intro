f984ba564c5bab0fe9fde3d671713e195d5b3b3b
RPatel <rpatel@linz.govt.nz>
2023-04-18 16:17:01 +1200

    Decouple front end from the use case layer as per clean architecture. https://toitutewhenua.atlassian.net/wiki/spaces/STEP/pages/155025826/Using+Clean+Architecture+in+a+React+Application

--- src/public-transport/TransportSelector.js
+++ src/public-transport/TransportSelector.js
@@ -1,47 +1,11 @@
-import {useState} from "react";
 import StopDepartures from "./StopDepartures";
-import { useQuery } from "@tanstack/react-query";
-import fetchStopPredictions from "./FetchStopPredictions";
-
-const MODES = ["Train", "Bus"];
-const TRAIN_STOPS = [{stop_id: "JOHN", name: "Johnsonville"}, {stop_id: "RARO", name: "Raroa"}, {stop_id: "WELL", name: "Wellington"}]
-const BUS_STOPS = [{stop_id: "3081", name: "Johnsonville Mall"}, {stop_id: "3252", name: "BP Johnsonville"},
-    {stop_id: "5012", name: "Lambton Central - Stop A"}, {stop_id: "5016", name: "Wellington Station - Stop E"}]
+import {useDeparturesList} from "./UseDeparturesList";
+import {MODES} from "./UseDeparturesList";
 
 function TransportSelector() {
 
-    const [apiKey, setApiKey] = useState("");
-    const [mode, setMode] = useState("");
-    const [stop, setStop] = useState("");
-    const [stops, setStops] = useState([]);
-    const [stopId, setStopId] = useState("");
-
-    if(!mode) {
-        const defaultMode = MODES[1];
-        setMode(defaultMode)
-        setUpStops(defaultMode)
-    }
-    const { isLoading, error, data} = useQuery(['stop-predictions', stopId, mode, apiKey],
-        fetchStopPredictions , {
-        refetchOnMount: false,
-        refetchOnWindowFocus: false,
-        staleTime: 20000,
-    });
-
-
-    function setUpStops(typeOfTransport){
-        let stopsFromTransport;
-        if (typeOfTransport === "Train") {
-            stopsFromTransport = TRAIN_STOPS;
-        } else if (typeOfTransport === "Bus") {
-            stopsFromTransport = BUS_STOPS;
-        }
-        setStops(stopsFromTransport);
-        // arbitrarily pre-select the 1st stop
-        const stopToSelect = stopsFromTransport[0];
-        setStopId(stopToSelect.stop_id);
-        setStop(stopToSelect.name)
-    }
+    const {apiKey, setApiKey, mode, setMode, stop, setStop, stops, setStopId, isLoading,
+        error, data, setUpStops} = useDeparturesList();
 
     function setStopIdFromEvent(event) {
         const selectedIndex = event.target.options.selectedIndex;
